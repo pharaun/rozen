05b449fa6f33521be55530cd482d2c3ecc2564e6
Anja Berens <pharaun666@gmail.com>
2021-08-21 00:19:39 -0700

    initial walker

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use ignore::WalkBuilder;
+
 fn main() {
-    println!("Hello, world!");
+    let target_dir = "tmp";
+    let follow_symlink = true;
+    let same_file_system = true;
+
+
+    // Do we want sort by filename? it will allow determistic order
+    for entry in WalkBuilder::new(target_dir)
+        .follow_links(follow_symlink)
+        .standard_filters(false)
+        .same_file_system(same_file_system)
+        .sort_by_file_name(|a, b| a.cmp(b))
+        .build() {
+
+        match entry {
+            Ok(e) => println!("{}", e.path().display()),
+            Err(e) => println!("{:?}", e),
+        }
+    }
 }
