23eb949edc73e26d4f4dc60b6c10f0e059e6108f
Anja Berens <pharaun666@gmail.com>
2021-08-21 02:11:56 -0700

    We compress in memory then shovel it into a tarball as a proof of concept

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 use ignore::WalkBuilder;
 
+use std::fs::File;
+use tar::{Builder, Header};
+use std::io::Read;
+
 fn main() {
     let target_dir = "tmp";
     let follow_symlink = true;
     let same_file_system = true;
 
+    let tar_file = File::create("test.tar").unwrap();
+    let mut tar = Builder::new(tar_file);
 
     // Do we want sort by filename? it will allow determistic order
     for entry in WalkBuilder::new(target_dir)
@@ -15,8 +21,59 @@ fn main() {
         .build() {
 
         match entry {
-            Ok(e) => println!("{}", e.path().display()),
-            Err(e) => println!("{:?}", e),
+            Ok(e) => {
+                match e.file_type() {
+                    None => println!("NONE: {}", e.path().display()),
+                    Some(ft) => {
+                        if ft.is_file() {
+                            // In memory compression, fine for now
+                            let file_data: Vec<u8> = {
+                                let mut content = Vec::new();
+
+                                std::fs::File::open(e.path())
+                                    .unwrap()
+                                    .read_to_end(&mut content)
+                                    .unwrap();
+                                content
+                            };
+                            let comp_data: Vec<u8> = zstd::encode_all(&file_data[..], 21).unwrap();
+
+                            if file_data.len() < comp_data.len() {
+                                // Pull in raw
+                                println!("RAWW: {}", e.path().display());
+
+                                // Header for the tar pack
+                                let mut header = Header::new_gnu();
+                                header.set_path(e.path()).unwrap();
+                                header.set_size(file_data.len() as u64);
+                                header.set_cksum();
+
+                                // stuff it into the tar now
+                                tar.append(&header, &file_data[..]).unwrap();
+
+                            } else {
+                                // Pull in COMP
+                                println!("COMP: {}", e.path().display());
+
+                                // Header for the tar pack
+                                let mut header = Header::new_gnu();
+                                header.set_path(e.path()).unwrap();
+                                header.set_size(comp_data.len() as u64);
+                                header.set_cksum();
+
+                                // stuff it into the tar now
+                                tar.append(&header, &comp_data[..]).unwrap();
+                            }
+                        } else {
+                            println!("SKIP: {}", e.path().display());
+                        }
+                    },
+                }
+            },
+            Err(e) => println!("ERRR: {:?}", e),
         }
     }
+
+    // Wrap up the tar file
+    let data = tar.into_inner().unwrap();
 }
