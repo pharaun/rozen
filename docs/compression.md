3080b20ffa34c300c6d2794cc8a55a843f72360c
Anja Berens <pharaun666@gmail.com>
2021-08-21 18:32:25 -0700

    chunk idea works well, its 1.6 vs 2.2 degradation on flat file compression

--- docs/compression.md
+++ docs/compression.md
@@ -1,36 +1,42 @@
 Compression:
-	tar  - 1.0G - 1s
-	lz4  - 373M - 2m 34s
-	lzo  - 349M - 3m 49s
-	gz   - 304M - 1m 43s
-	dar  - 294M - 1m 11s (Tar+bz2 replacement)
-	bz2  - 287M - 58s
-	dar  - 260M - 5m 59 (Tar+zstd replacement)
-	zst  - 160M - 5m 6s (zstd)
-	lz   - 160M - 6m 53s (lzip - lzma based)
-	xz   - 153M - 7m 3s (lzma/xz)
-	zst  - 136M - 5m 39s (zstd wlog=31,strat=9,clog=30,slog=30)
-	lrz  - 136M - 33s (zpaq failed) (long dictionary lzma) (lrzip)
-	zpaq - 135M - 9m 43s
-	br   - 126M - 31m 15s (brotli)
+    tar  - 1.0G - 1s
+    lz4  - 373M - 2m 34s
+    lzo  - 349M - 3m 49s
+    gz   - 304M - 1m 43s
+    dar  - 294M - 1m 11s (Tar+bz2 replacement)
+    bz2  - 287M - 58s
+    dar  - 260M - 5m 59 (Tar+zstd replacement)
+    zst  - 160M - 5m 6s (zstd)
+    lz   - 160M - 6m 53s (lzip - lzma based)
+    xz   - 153M - 7m 3s (lzma/xz)
+    zst  - 136M - 5m 39s (zstd wlog=31,strat=9,clog=30,slog=30)
+    lrz  - 136M - 33s (zpaq failed) (long dictionary lzma) (lrzip)
+    zpaq - 135M - 9m 43s
+    br   - 126M - 31m 15s (brotli)
+
+Chunk Compression:
+    tar            - 1.2G
+    per file       - 332M
+    per 10Mb chunk - 246M
+    tar.zst        - 150M
 
 Deduplication:
-	Works:
-		fpgaminer/preserve - 1.0G (Claims to compress with xz and stuff but?)
-		restic/restic - 995M (No compression of blocks)
-		andrewchambers/bupstash - 456M
-		sourcefrog/conserve - 455M
-		dpc/rdedup - 294M
-		bup/bup - 294M
-		asuran-rs/asuran - 261M (zstd,22)
-		borgbackup/borg - 241M (zstd,22)
+    Works:
+        fpgaminer/preserve - 1.0G (Claims to compress with xz and stuff but?)
+        restic/restic - 995M (No compression of blocks)
+        andrewchambers/bupstash - 456M
+        sourcefrog/conserve - 455M
+        dpc/rdedup - 294M
+        bup/bup - 294M
+        asuran-rs/asuran - 261M (zstd,22)
+        borgbackup/borg - 241M (zstd,22)
 
-	Busted:
-		zbackup/zbackup - (Complicated installation, no thanks for now)
-		basak/ddar - (Python something couldn't get it setup, i think py2.7?)
-		derekp7/snebu - (Client/server setup, kinda complicated to get going?)
-		kopia/kopia - (Works but is unclear what it all backed up)
+    Busted:
+        zbackup/zbackup - (Complicated installation, no thanks for now)
+        basak/ddar - (Python something couldn't get it setup, i think py2.7?)
+        derekp7/snebu - (Client/server setup, kinda complicated to get going?)
+        kopia/kopia - (Works but is unclear what it all backed up)
 
-	TBD:
-		duplicati/duplicati
-		gilbertchen/duplicacy
+    TBD:
+        duplicati/duplicati
+        gilbertchen/duplicacy
--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,30 @@ use ignore::WalkBuilder;
 use std::fs::File;
 use tar::{Builder, Header};
 use std::io::Read;
-use std::io::Write;
 
 fn main() {
     let target_dir = "tmp";
     let follow_symlink = true;
     let same_file_system = true;
 
-    let tar_file = File::create("test.tar").unwrap();
-    let mut tar = Builder::new(tar_file);
 
-    let mut test_file = File::create("test.file").unwrap();
+    // Enclosuring tarfile
+    let tar_file = File::create("test.tar").unwrap();
+    let mut enc_tar = Builder::new(tar_file);
+
+
+    // Chunked tarfile
+    let limit = 1024 * 1024 * 10;
+    let mut chunk_len = 0;
+    let mut chunk_idx: u32 = 0;
+
+    let mut chunk_tar = Builder::new(Vec::new());
+
+
+    // Compare this to each file get a zstd compressed
+    let cmp_tar_file = File::create("test2.tar").unwrap();
+    let mut cmp_tar = Builder::new(cmp_tar_file);
+
 
     // Do we want sort by filename? it will allow determistic order
     for entry in WalkBuilder::new(target_dir)
@@ -29,47 +42,83 @@ fn main() {
                     None => println!("NONE: {}", e.path().display()),
                     Some(ft) => {
                         if ft.is_file() {
-                            // In memory compression, fine for now
-                            let file_data: Vec<u8> = {
-                                let mut content = Vec::new();
+                            // 1. ingest it into chunk_tar
+                            {
+                                println!("EATS: {}", e.path().display());
 
-                                std::fs::File::open(e.path())
-                                    .unwrap()
-                                    .read_to_end(&mut content)
-                                    .unwrap();
-                                content
-                            };
-                            let comp_data: Vec<u8> = zstd::encode_all(&file_data[..], 21).unwrap();
+                                let file_data: Vec<u8> = {
+                                    let mut content = Vec::new();
 
-                            if file_data.len() < comp_data.len() {
-                                // Pull in raw
-                                println!("RAWW: {}", e.path().display());
+                                    std::fs::File::open(e.path())
+                                        .unwrap()
+                                        .read_to_end(&mut content)
+                                        .unwrap();
+                                    content
+                                };
+                                chunk_len += file_data.len();
 
-                                // Header for the tar pack
+                                // Header for each file into the chunk
                                 let mut header = Header::new_gnu();
                                 header.set_size(file_data.len() as u64);
                                 header.set_cksum();
 
                                 // stuff it into the tar now
-                                tar.append_data(&mut header, e.path(), &file_data[..]).unwrap();
+                                chunk_tar.append_data(
+                                    &mut header,
+                                    e.path(),
+                                    &file_data[..]
+                                ).unwrap();
 
-                                // Dump it to the file too
-                                test_file.write(&file_data[..]);
 
-                            } else {
-                                // Pull in COMP
-                                println!("COMP: {}", e.path().display());
+                                // COMPARE
+                                {
+                                    header = Header::new_gnu();
+                                    header.set_size(file_data.len() as u64);
+                                    header.set_cksum();
 
-                                // Header for the tar pack
+                                    let comp_data: Vec<u8> = zstd::encode_all(
+                                        &file_data[..],
+                                        21
+                                    ).unwrap();
+
+                                    // stuff it into the tar now
+                                    cmp_tar.append_data(
+                                        &mut header,
+                                        e.path(),
+                                        &comp_data[..]
+                                    ).unwrap();
+                                }
+                            }
+
+                            // 2. check if chunk_tar is greater than limit
+                            if chunk_len > limit {
+                                println!("STOW: {} chunk", chunk_idx);
+
+                                // 3. if so, finalize it.
+                                let chunk = chunk_tar.into_inner().unwrap();
+
+                                // 4. compress it with zstd
+                                let comp_data: Vec<u8> = zstd::encode_all(
+                                    &chunk[..],
+                                    21
+                                ).unwrap();
+
+                                // 5. ingest the compressed chunk into enc_tar
+                                let chunk_name = format!("{}.tar.zst", chunk_idx);
+
+                                // Header for the new chunk
                                 let mut header = Header::new_gnu();
                                 header.set_size(comp_data.len() as u64);
+                                header.set_path(chunk_name).unwrap();
                                 header.set_cksum();
 
                                 // stuff it into the tar now
-                                tar.append_data(&mut header, e.path(), &comp_data[..]).unwrap();
+                                enc_tar.append(&header, &comp_data[..]).unwrap();
 
-                                // Dump it to the file too
-                                test_file.write(&comp_data[..]);
+                                // 6. re-make the chunk+chunk_tar for next batch
+                                chunk_len = 0;
+                                chunk_tar = Builder::new(Vec::new());
+                                chunk_idx += 1;
                             }
                         } else {
                             println!("SKIP: {}", e.path().display());
@@ -81,6 +130,35 @@ fn main() {
         }
     }
 
+    // 7. on completion if there is data in chunk, finalize it
+    if chunk_len > 0 {
+        println!("TRAILER: {} chunk", chunk_idx);
+
+        // 3. if so, finalize it.
+        let chunk = chunk_tar.into_inner().unwrap();
+
+        // 4. compress it with zstd
+        let comp_data: Vec<u8> = zstd::encode_all(
+            &chunk[..],
+            21
+        ).unwrap();
+
+        // 5. ingest the compressed chunk into enc_tar
+        let chunk_name = format!("{}.tar.zst", chunk_idx);
+
+        // Header for the new chunk
+        let mut header = Header::new_gnu();
+        header.set_size(comp_data.len() as u64);
+        header.set_path(chunk_name).unwrap();
+        header.set_cksum();
+
+        // stuff it into the tar now
+        enc_tar.append(&header, &comp_data[..]).unwrap();
+    }
+
     // Wrap up the tar file
-    let data = tar.into_inner().unwrap();
+    let _ = enc_tar.into_inner().unwrap();
+
+    // Wrap compare tar file
+    let _ = cmp_tar.into_inner().unwrap();
 }
