31495fb0e1526c1f8c1cbca937676f8e5c0546dd
Anja Berens <pharaun666@gmail.com>
2022-02-12 16:51:13 -0800

    Move archive out into append only engine for now, further abstraction can happen in due time

---
+++ src/engine/append_only.rs
@@ -0,0 +1,85 @@
+use std::io::{Seek, SeekFrom, copy, Read};
+use blake3::Hasher;
+use blake3::Hash;
+use zstd::stream::read::Encoder;
+
+use crate::index::Index;
+use crate::crypto;
+use crate::backend::mem::Backend;
+
+
+pub fn archive<B: Backend>(
+    key: &crypto::Key,
+    backend: &mut B,
+    walker: ignore::Walk,
+) {
+    let index = Index::new();
+
+    {
+        for entry in walker {
+            match entry {
+                Ok(e) => {
+                    match e.file_type() {
+                        None => println!("NONE: {}", e.path().display()),
+                        Some(ft) => {
+                            if ft.is_file() {
+                                println!("COMP: {}", e.path().display());
+
+                                let mut file_data = std::fs::File::open(e.path()).unwrap();
+
+                                // Hasher
+                                let content_hash = hash(
+                                    &key,
+                                    &mut file_data
+                                ).unwrap().to_hex().to_string();
+
+                                // Streaming compressor
+                                file_data.seek(SeekFrom::Start(0)).unwrap();
+
+                                let comp = Encoder::new(
+                                    &mut file_data,
+                                    21
+                                ).unwrap();
+
+                                // Encrypt the stream
+                                let mut enc = crypto::encrypt(&key, comp).unwrap();
+
+                                // Stream the data into the backend
+                                let mut write_to = backend.write(content_hash.as_str()).unwrap();
+                                copy(&mut enc, &mut write_to).unwrap();
+
+                                // Load file info into index
+                                index.insert_file(e.path(), content_hash.as_str());
+                            } else {
+                                println!("SKIP: {}", e.path().display());
+                            }
+                        },
+                    }
+                },
+                Err(e) => println!("ERRR: {:?}", e),
+            }
+        }
+
+        // Spool the sqlite file into the backend as index
+        let mut s_file = index.unload();
+
+        let comp = Encoder::new(
+            &mut s_file,
+            21
+        ).unwrap();
+
+        // Encrypt the stream
+        let mut enc = crypto::encrypt(&key, comp).unwrap();
+
+        // Stream the data into the backend
+        let mut write_to = backend.write("INDEX.sqlite.zst").unwrap();
+        copy(&mut enc, &mut write_to).unwrap();
+    }
+}
+
+// copy paste from main.rs for now
+fn hash<R: Read>(key: &crypto::Key, data: &mut R) -> Result<Hash, std::io::Error> {
+    let mut hash = Hasher::new_keyed(&key.0);
+    copy(data, &mut hash)?;
+    Ok(hash.finalize())
+}
--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,6 @@ mod engine;
 mod index;
 use crate::index::Index;
 
-
 // Configuration
 // At a later time honor: https://aws.amazon.com/blogs/security/a-new-and-standardized-way-to-manage-credentials-in-the-aws-sdks/
 // envy = "0.4.2" - for grabbing the env vars via serde
@@ -42,7 +41,7 @@ enum SourceType {
 }
 
 fn main() {
-    crypto::init();
+    crypto::init().unwrap();
 
     // Per run key
     let key = crypto::gen_key();
@@ -66,75 +65,18 @@ fn main() {
     // In memory backend for data storage
     let mut backend = backend::mem::MemoryVFS::new();
 
-    let index = Index::new();
-    {
-        // Sort filename for determistic order
-        for entry in WalkBuilder::new(target)
+    engine::append_only::archive(
+        &key,
+        &mut backend,
+        WalkBuilder::new(target)
             .follow_links(config.symlink)
             .standard_filters(false)
             .same_file_system(config.same_fs)
             .sort_by_file_name(|a, b| a.cmp(b))
-            .build() {
+            .build(),
+    );
 
-            match entry {
-                Ok(e) => {
-                    match e.file_type() {
-                        None => println!("NONE: {}", e.path().display()),
-                        Some(ft) => {
-                            if ft.is_file() {
-                                println!("COMP: {}", e.path().display());
-
-                                let mut file_data = std::fs::File::open(e.path()).unwrap();
-
-                                // Hasher
-                                let content_hash = hash(
-                                    &key,
-                                    &mut file_data
-                                ).unwrap().to_hex().to_string();
-
-                                // Streaming compressor
-                                file_data.seek(SeekFrom::Start(0)).unwrap();
-
-                                let comp = Encoder::new(
-                                    &mut file_data,
-                                    21
-                                ).unwrap();
-
-                                // Encrypt the stream
-                                let mut enc = crypto::encrypt(&key, comp).unwrap();
-
-                                // Stream the data into the backend
-                                let mut write_to = backend.write(content_hash.as_str()).unwrap();
-                                copy(&mut enc, &mut write_to).unwrap();
-
-                                // Load file info into index
-                                index.insert_file(e.path(), content_hash.as_str());
-                            } else {
-                                println!("SKIP: {}", e.path().display());
-                            }
-                        },
-                    }
-                },
-                Err(e) => println!("ERRR: {:?}", e),
-            }
-        }
-
-        // Spool the sqlite file into the backend as index
-        let mut s_file = index.unload();
-
-        let comp = Encoder::new(
-            &mut s_file,
-            21
-        ).unwrap();
-
-        // Encrypt the stream
-        let mut enc = crypto::encrypt(&key, comp).unwrap();
-
-        // Stream the data into the backend
-        let mut write_to = backend.write("INDEX.sqlite.zst").unwrap();
-        copy(&mut enc, &mut write_to).unwrap();
-    }
-
+    let index = Index::new();
     println!("\nARCHIVE Dump");
     for k in backend.list_keys().unwrap() {
         let mut read_from = backend.read(&k).unwrap();
@@ -171,6 +113,7 @@ fn main() {
 }
 
 
+// copy pasted into appendonly for now
 fn hash<R: Read>(key: &crypto::Key, data: &mut R) -> Result<Hash, std::io::Error> {
     let mut hash = Hasher::new_keyed(&key.0);
     copy(data, &mut hash)?;
