9cd855d945cb1ca8c41aacbc6fd73fca6baf82cb
Anja Berens <pharaun666@gmail.com>
2022-03-25 16:02:12 -0700

    Tested Ltvc raw streamer

--- src/ltvc.rs
+++ src/ltvc.rs
@@ -580,4 +580,76 @@ mod test_ltvc_raw_iterator {
         );
         assert!(reader.next().is_none());
     }
+
+    #[test]
+    fn one_pseudofile() {
+        let test_data_fhdr = vec![1, 2, 3, 4];
+        let test_data_fidx = vec![5, 6, 7, 8];
+
+        // Write to the stream
+        let data = Cursor::new(Vec::new());
+        let hash = test_hash();
+        let mut edat1 = Cursor::new(test_data_fhdr.clone());
+        let mut edat2 = Cursor::new(test_data_fidx.clone());
+        let mut builder = LtvcBuilder::new(data);
+
+        builder.write_ahdr(0x01).unwrap();
+        builder.write_fhdr(&hash).unwrap();
+        builder.write_edat(&mut edat1).unwrap();
+        builder.write_fidx().unwrap();
+        builder.write_edat(&mut edat2).unwrap();
+        builder.write_aend(910).unwrap();
+
+        // Reset stream
+        let mut data = builder.to_inner();
+        data.seek(SeekFrom::Start(0)).unwrap();
+
+        // Read back and assert stuff
+        let mut reader = LtvcReaderRaw::new(data);
+
+        // This is what a small one fhdr+fidx file should look like
+        assert_eq!(
+            LtvcEntryRaw {
+                typ: *b"AHDR",
+                data: vec![0x01],
+            },
+            reader.next().unwrap().unwrap()
+        );
+        assert_eq!(
+            LtvcEntryRaw {
+                typ: *b"FHDR",
+                data: hash.as_bytes().to_vec(),
+            },
+            reader.next().unwrap().unwrap()
+        );
+        assert_eq!(
+            LtvcEntryRaw {
+                typ: *b"EDAT",
+                data: test_data_fhdr,
+            },
+            reader.next().unwrap().unwrap()
+        );
+        assert_eq!(
+            LtvcEntryRaw {
+                typ: *b"FIDX",
+                data: vec![],
+            },
+            reader.next().unwrap().unwrap()
+        );
+        assert_eq!(
+            LtvcEntryRaw {
+                typ: *b"EDAT",
+                data: test_data_fidx,
+            },
+            reader.next().unwrap().unwrap()
+        );
+        assert_eq!(
+            LtvcEntryRaw {
+                typ: *b"AEND",
+                data: vec![142, 3, 0, 0],
+            },
+            reader.next().unwrap().unwrap()
+        );
+        assert!(reader.next().is_none());
+    }
 }
