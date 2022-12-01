a3e5ed1e2b0a11ae01e2d7c967d92d7961d47913
Anja Berens <pharaun666@gmail.com>
2022-12-01 03:56:15 -0800

    Initial attempt of a CAS system

--- src/append.rs
+++ src/append.rs
@@ -31,26 +31,20 @@ pub fn snapshot<B: Backend>(
     walker: ignore::Walk,
 ) {
     let index = Index::new();
-    let mut wpack = None;
-
-    let mut map = {
-        let map_id = mapper::generate_map_id();
-        let multiwrite = backend.write_multi(&map_id).unwrap();
-        MapBuilder::new(map_id, multiwrite)
-    };
+    let mut cas = ObjectStore::new(backend);
 
     {
         for entry in walker {
             match entry {
                 Ok(e) => {
-                    let meta = e.metadata().unwrap();
-                    println!("len: {:?}", meta.len());
-
                     match e.file_type() {
                         None => println!("NONE: {}", e.path().display()),
                         Some(ft) => {
                             if ft.is_file() {
                                 println!("COMP: {}", e.path().display());
+
+                                let meta = e.metadata().unwrap();
+                                println!("len: {:?}", meta.len());
 
                                 let mut file_data = std::fs::File::open(e.path()).unwrap();
 
@@ -75,35 +69,12 @@ pub fn snapshot<B: Backend>(
                                 // Encrypt the stream
                                 let mut enc = crypto::encrypt(&key, comp).unwrap();
 
-                                // Stream the data into the pack
-                                // TODO:
-                                //  1. compression complicates things for things below a certain
-                                //     size don't bother compressing?
-                                //  2. for things above a size try compressing
-                                //  3. See if i can't do fast bail out but would require
-                                //     spooling....
-                                //  4. If below certain size go ahead and pack it up
-                                //  5. if above certain size just send it as its own archive to
-                                //     backend
-                                let mut chunker = chunk::Chunk::new(&mut enc);
-                                let mut pack_id: HashSet<hash::Hash> = HashSet::new();
-
-                                while let Some((mut chunk, part)) = chunker.next() {
-                                    let t_wpack = wpack.get_or_insert_with(|| {
-                                        let pack_id = pack::generate_pack_id();
-                                        let multiwrite = backend.write_multi(&pack_id).unwrap();
-                                        PackBuilder::new(pack_id, multiwrite)
-                                    });
-                                    pack_id.insert(t_wpack.id.clone());
-
-                                    if t_wpack.append(
-                                        content_hash.clone(),
-                                        part,
-                                        &mut chunk
-                                    ) {
-                                        wpack.take().unwrap().finalize(&mut map, &key);
-                                    }
-                                }
+                                // Stream the data into the CAS system
+                                let pack_id = cas.append(
+                                    content_hash.clone(),
+                                    &key,
+                                    &mut enc
+                                );
 
                                 // Load file info into index
                                 // Snapshot will be '<packfile-id>:<hash-id>' to pull out
@@ -126,13 +97,8 @@ pub fn snapshot<B: Backend>(
             }
         }
 
-        // Force an finalize if its not already finalized
-        if wpack.is_some() {
-            wpack.take().unwrap().finalize(&mut map, &key);
-        }
-
-        // Finalize the mapper
-        map.finalize(&key);
+        // Finalize the CAS
+        cas.finalize(&key);
 
         // Spool the sqlite file into the backend as index
         // TODO: update this to support the archive file format defined in pack.rs
@@ -151,5 +117,82 @@ pub fn snapshot<B: Backend>(
         let filename = format!("INDEX-{}.sqlite.zst", dt_fmt);
         println!("INDEX: {:?}", filename);
         backend.write_filename(&filename, &mut enc).unwrap();
+    }
+}
+
+
+use std::io::Read;
+use std::io::Write;
+
+// This manages the under laying layer
+// - chunking
+// - packfiles
+// - hash -> packfile+chunks
+//
+// This then presents a nice view to the user of this, which let them give a hash and a stream to
+// write or a hash and a read to read from
+pub struct ObjectStore<'a, B: Backend + 'a> {
+    w_backend: &'a B,
+
+    w_pack: Option<PackBuilder<Box<dyn Write>>>,
+    w_map: MapBuilder<Box<dyn Write>>,
+}
+
+impl<'a, B: Backend> ObjectStore<'a, B> {
+    pub fn new(backend: &'a mut B) -> Self {
+        let map = {
+            let map_id = mapper::generate_map_id();
+            let multiwrite = backend.write_multi(&map_id).unwrap();
+            MapBuilder::new(map_id, multiwrite)
+        };
+
+        ObjectStore {
+            w_backend: backend,
+            w_pack: None,
+            w_map: map,
+        }
+    }
+
+    pub fn append<R: Read>(&mut self, hash: hash::Hash, key: &crypto::Key, reader: &mut R) -> HashSet<hash::Hash> {
+        // Stream the data into the pack
+        // TODO:
+        //  1. compression complicates things for things below a certain
+        //     size don't bother compressing?
+        //  2. for things above a size try compressing
+        //  3. See if i can't do fast bail out but would require
+        //     spooling....
+        //  4. If below certain size go ahead and pack it up
+        //  5. if above certain size just send it as its own archive to
+        //     backend
+        let mut chunker = chunk::Chunk::new(reader);
+        let mut pack_id: HashSet<hash::Hash> = HashSet::new();
+
+        while let Some((mut chunk, part)) = chunker.next() {
+            let t_pack = self.w_pack.get_or_insert_with(|| {
+                let pack_id = pack::generate_pack_id();
+                let multiwrite = self.w_backend.write_multi(&pack_id).unwrap();
+                PackBuilder::new(pack_id, multiwrite)
+            });
+            pack_id.insert(t_pack.id.clone());
+
+            if t_pack.append(
+                hash.clone(),
+                part,
+                &mut chunk
+            ) {
+                self.w_pack.take().unwrap().finalize(&mut self.w_map, &key);
+            }
+        }
+
+        pack_id
+    }
+
+    pub fn finalize(mut self, key: &crypto::Key) {
+        // Force an finalize if its not already finalized
+        if self.w_pack.is_some() {
+            self.w_pack.take().unwrap().finalize(&mut self.w_map, &key);
+        }
+
+        self.w_map.finalize(&key);
     }
 }
