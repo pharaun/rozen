2d63a1d51790feda1c3c8274a077e8b1866b315d
Anja Berens <pharaun666@gmail.com>
2025-03-08 21:28:34 -0800

    Test a bunch of serialization choices

--- rarc/src/ltvc/indexing.rs
+++ rarc/src/ltvc/indexing.rs
@@ -9,13 +9,43 @@ use rcore::key;
 
 use crate::ltvc::builder::LtvcBuilder;
 
+// Test imports
+use binrw::binrw;
+use borsh::BorshDeserialize;
+use borsh::BorshSerialize;
+
 // TODO: add header type, for initial impl this is Fidx only
+#[binrw]
+#[brw(little)]
 #[derive(Serialize, Deserialize, Debug)]
+#[derive(BorshSerialize, BorshDeserialize)]
 pub struct HeaderIdx {
     pub typ: [u8; 4],
+
+    #[bw(map = |x| x.as_bytes())]
+    #[br(map = |x: [u8; 32]| x.into())]
+    #[borsh(deserialize_with = "deserialize_hash", serialize_with = "serialize_hash")]
     pub hash: hash::Hash,
+
+    #[bw(map = |x| *x as u32)]
+    #[br(map = |x: u32| x as usize)]
     pub start_idx: usize,
+
+    #[bw(map = |x| *x as u32)]
+    #[br(map = |x: u32| x as usize)]
     pub length: usize,
+}
+
+// Borsh
+fn deserialize_hash<R: borsh::io::Read>(reader: &mut R) -> Result<hash::Hash, borsh::io::Error> {
+    let hash: [u8; 32] = borsh::BorshDeserialize::deserialize_reader(reader)?;
+    Ok(hash.into())
+}
+
+fn serialize_hash<W: borsh::io::Write>(hash: &hash::Hash, writer: &mut W) -> Result<(), borsh::io::Error> {
+    let hash: &[u8; 32] = hash.as_bytes();
+    borsh::BorshSerialize::serialize(hash, writer)?;
+    Ok(())
 }
 
 pub struct LtvcIndexing<W: Write> {
@@ -110,28 +140,96 @@ impl<W: Write> LtvcIndexing<W> {
 mod serialize {
     use super::HeaderIdx;
 
+    use std::io::Cursor;
+    use binrw::BinWrite;
+    use bcs::to_bytes;
+
     use rcore::key::MemKey;
 
-    #[test]
-    fn small_data_roundtrip() {
-        let idx: Vec<HeaderIdx> = {
-            let key = MemKey::new();
+    fn data_gen() -> Vec<HeaderIdx> {
+        let key = MemKey::new();
 
-            vec![
-                HeaderIdx {
-                    typ: *b"FHDR",
-                    hash: key.gen_id(),
-                    start_idx: 0,
-                    length: 10,
-                }
-            ]
-        };
+        vec![
+            HeaderIdx {
+                typ: *b"FHDR",
+                hash: key.gen_id(),
+                start_idx: 0,
+                length: 10,
+            }
+        ]
+    }
+
+    #[test]
+    fn small_data_bincode() {
+        let idx = data_gen();
 
         // Test encode options
         let config = bincode::config::standard().with_little_endian().with_variable_int_encoding();
         let index = bincode::serde::encode_to_vec(&idx, config).unwrap();
 
-        println!("{:x?}", index);
+        println!("{}", hex::encode(&index));
+
+        assert!(false);
+    }
+
+    #[test]
+    fn small_data_cbor() {
+        let idx = data_gen();
+
+        // Test encode options
+        let mut index = Vec::new();
+        ciborium::into_writer(&idx, &mut index).unwrap();
+
+        println!("{}", hex::encode(&index));
+
+        assert!(false);
+    }
+
+    #[test]
+    fn small_data_msgpack() {
+        let idx = data_gen();
+
+        // Test encode options
+        let index = rmp_serde::to_vec(&idx).unwrap();
+
+        println!("{}", hex::encode(&index));
+
+        assert!(false);
+    }
+
+    #[test]
+    fn small_data_binrw() {
+        let idx = data_gen();
+
+        // Test encode options
+        let mut index = Cursor::new(Vec::new());
+        idx.write(&mut index).unwrap();
+
+        println!("{}", hex::encode(index.into_inner()));
+
+        assert!(false);
+    }
+
+    #[test]
+    fn small_data_bcs() {
+        let idx = data_gen();
+
+        // Test encode options
+        let index = to_bytes(&idx).unwrap();
+
+        println!("{}", hex::encode(index));
+
+        assert!(false);
+    }
+
+    #[test]
+    fn small_data_borsh() {
+        let idx = data_gen();
+
+        // Test encode options
+        let index = borsh::to_vec(&idx).unwrap();
+
+        println!("{}", hex::encode(index));
 
         assert!(false);
     }
